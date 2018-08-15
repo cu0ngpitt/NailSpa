e56e9d343d2993879456e24bbfd85d2da021df41
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-08-15 17:25:18 -0500

    Imported components and added rounting links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
+const appRoutes: Routes = [
+  { path: 'home',     component: HomeComponent },
+  { path: 'contact',  component: ContactComponent },
+  { path: 'services',  component: ServicesComponent },
+  { path: 'gallery',  component: GalleryComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
