ca7bc0310ad68419371c2a2d86f132569cae47a0
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-08-19 17:00:25 -0500

    Added routing functionality to the pricing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { PricingComponent } from './components/pricing/pricing.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const appRoutes: Routes = [
   { path: 'home',     component: HomeComponent },
-  { path: 'contact',  component: ContactComponent },
   { path: 'services',  component: ServicesComponent },
+  { path: 'pricing',  component: PricingComponent },
   { path: 'gallery',  component: GalleryComponent },
+  { path: 'contact',  component: ContactComponent },
   { path: '',
     redirectTo: '/home',
     pathMatch: 'full'
