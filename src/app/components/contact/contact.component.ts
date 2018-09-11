a20d1df0dbf4f9150b0b8027400e1c0b7e15df1a
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-09-10 19:22:39 -0500

    Removed class used for testing

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  prefContact: any;
 
   constructor() { }
 
