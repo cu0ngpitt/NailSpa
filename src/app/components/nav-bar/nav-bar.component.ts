94fb5fe747d56af86dfd55ca133b1636e9e028b6
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-09-11 20:08:35 -0500

    Created side bar for mobile viewing

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  toggle = false;
+  drop = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleNavbar(toggle) {
+    if (toggle === false) {
+      this.toggle = true;
+    } else if (toggle === true) {
+      this.toggle = false;
+    }
+  }
+
+  toggleDropdown(drop) {
+    if (drop === false) {
+      this.drop = true;
+    } else if (drop === true) {
+      this.drop= false;
+    }
+  }
 }
