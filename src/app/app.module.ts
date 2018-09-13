cd087739147f7fd54dd6ddcb59144531179876a5
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-09-12 19:05:01 -0500

    Imported angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule }            from '@angular/platform-browser';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { NgModule }                 from '@angular/core';
+import { RouterModule, Routes }     from '@angular/router';
+import { FormsModule }              from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AppComponent }       from './app.component';
+import { NavBarComponent }    from './components/nav-bar/nav-bar.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
-import { HomeComponent } from './components/home/home.component';
-import { ServicesComponent } from './components/services/services.component';
-import { PricingComponent } from './components/pricing/pricing.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent }      from './components/home/home.component';
+import { ServicesComponent }  from './components/services/services.component';
+import { PricingComponent }   from './components/pricing/pricing.component';
+import { GalleryComponent }   from './components/gallery/gallery.component';
+import { ContactComponent }   from './components/contact/contact.component';
 
 const appRoutes: Routes = [
   { path: 'home',     component: HomeComponent },
@@ -39,6 +40,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(
       appRoutes,
       { scrollPositionRestoration: 'enabled' }
