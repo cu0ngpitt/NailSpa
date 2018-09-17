429fd4d07e9997abb9d2c83b947f199d7755e668
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-09-17 14:20:18 -0500

    Added a back to top button

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
@@ -12,4 +12,21 @@ export class PricingComponent implements OnInit {
   ngOnInit() {
   }
 
+  @HostListener("window:scroll", []) onWindowScroll() {
+    this.scrollFunction();
+  }
+  // When the user scrolls down 20px from the top of the document, show the button
+  scrollFunction() {
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+          document.getElementById("myBtn").className = "to-top show";
+      } else {
+          document.getElementById("myBtn").className = "to-top hidden";
+      }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 }
