b95358ac764ab56f38857fe41d1870021cf35170
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-09-17 14:48:30 -0500

    Added smooth scrolling to top

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -26,7 +26,10 @@ export class PricingComponent implements OnInit {
 
   // When the user clicks on the button, scroll to the top of the document
   topFunction() {
+      /*
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      */
+      window.scrollTo({top: 0, behavior: "smooth"});
   }
 }
