b3fb6c0f1601f5ab9d0d7cc7439c528c377a05e8
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-10-15 00:26:56 -0500

    Added a function to close the contact sidebar when clicking outside

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -4,17 +4,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SidebarService {
-  public state = 'inactive';
+  public state = 'inactive';  //for navbar sidebar
+  public show = false;        //for contact sidebar
 
   constructor() { }
+
+  contactBox() {
+    if(this.show === false) {
+      this.show = true;
+      document.getElementById('navbarNav').style.width = '0';
+    } else if (this.show === true) {
+      this.show = false;
+    }
+  }
 
   toggleNavbar() {
     if (this.state === 'inactive') {
       document.getElementById('navbarNav').style.width = '250px';
       this.state = 'active';
+      this.show = false;
     } else if (this.state === 'active') {
       document.getElementById('navbarNav').style.width = '0';
       this.state = 'inactive';
+    }
+  }
+
+  closeContactBox() {
+    if(this.show === true) {
+      this.show = false;
     }
   }
 }
