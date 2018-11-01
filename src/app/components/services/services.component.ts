8185d3f9362c25d6971844f01d2ef1d10bc54c17
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-11-01 03:31:21 -0500

    Refactored code to iterate price data

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 
+import { Nails } from '../../models/nails';
+import { Polish } from '../../models/polish';
+import { Price } from '../../models/price';
+
+
 import { SidebarService } from '../../services/sidebar.service';
+import { PricesService } from '../../services/prices.service';
 
 @Component({
   selector: 'app-services',
@@ -10,9 +16,35 @@ import { SidebarService } from '../../services/sidebar.service';
 export class ServicesComponent implements OnInit {
   show: boolean = false;
 
-  constructor(public sidebar: SidebarService) { }
+  /* Nails Data */
+  nails: Nails[];
+  dips: Nails[];
+  regPolish: Polish[];
+  gelPolish: Polish[];
+  others: Nails[];
+
+  /* Pedicure Data */
+  pedicure: Price[];
+
+  /* Manicure Data */
+  manicure: Price[];
+
+  /* Waxing Data */
+  waxing: Price[];
+
+  /* For Kids Data */
+  kids: Price[];
+  kidPolish: Nails[];
+
+  constructor(public sidebar: SidebarService,
+              private prices: PricesService) { }
 
   ngOnInit() {
+    this.getNails();
+    this.getPedicure();
+    this.getManicure();
+    this.getWax();
+    this.getKids();
   }
 
   scrollTo(link) {
@@ -24,4 +56,44 @@ export class ServicesComponent implements OnInit {
     handleScroll() {
       this.show = window.pageYOffset > 0;
     }
+
+  getNails(): void {
+    this.prices.getNails()
+      .subscribe((nail: any) => {
+        this.nails = nail.acrylic;
+        this.dips = nail.dip;
+        this.regPolish = nail.regPolish;
+        this.gelPolish = nail.gelPolish;
+        this.others = nail.other;
+      });
+  }
+
+  getPedicure(): void {
+    this.prices.getPedicure()
+      .subscribe((pedi: any) => {
+        this.pedicure = pedi.pedi;
+      });
+  }
+
+  getManicure(): void {
+    this.prices.getManicure()
+      .subscribe((mani: any) => {
+        this.manicure = mani.mani;
+      });
+  }
+
+  getWax(): void {
+    this.prices.getWax()
+      .subscribe((wax: any) => {
+        this.waxing = wax.wax;
+      });
+  }
+
+  getKids(): void {
+    this.prices.getKids()
+      .subscribe((kids: any) => {
+        this.kids = kids.kids;
+        this.kidPolish = kids.kidPolish;
+      });
+  }
 }
